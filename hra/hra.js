5675bcbf607194a1ab8c0453e1d19010cc6891f9
annasve <svejdovaam@gmail.com>
2023-10-11 16:40:19 +0200

    oživení všech herních políček

--- hra/hra.js
+++ hra/hra.js
@@ -21,34 +21,8 @@ const playerMove = (evt) => {
   evt.target.disabled = true; //(zamezení akce při kliknutí na již vybrané políčko)
 };
 
-//eventlisteners na herní políčka (button x 10)
-document
-  .querySelector('.playing-board__button:nth-child(1)')
-  .addEventListener('click', playerMove);
-document
-  .querySelector('.playing-board__button:nth-child(2)')
-  .addEventListener('click', playerMove);
-document
-  .querySelector('.playing-board__button:nth-child(3)')
-  .addEventListener('click', playerMove);
-document
-  .querySelector('.playing-board__button:nth-child(4)')
-  .addEventListener('click', playerMove);
-document
-  .querySelector('.playing-board__button:nth-child(5)')
-  .addEventListener('click', playerMove);
-document
-  .querySelector('.playing-board__button:nth-child(6)')
-  .addEventListener('click', playerMove);
-document
-  .querySelector('.playing-board__button:nth-child(7)')
-  .addEventListener('click', playerMove);
-document
-  .querySelector('.playing-board__button:nth-child(8)')
-  .addEventListener('click', playerMove);
-document
-  .querySelector('.playing-board__button:nth-child(9)')
-  .addEventListener('click', playerMove);
-document
-  .querySelector('.playing-board__button:nth-child(10)')
-  .addEventListener('click', playerMove);
+const playingBoard = document.querySelectorAll('.playing-board__button');
+
+playingBoard.forEach((item) => {
+  item.addEventListener('click', playerMove);
+});
